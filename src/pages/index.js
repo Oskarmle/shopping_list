f8671390c6d92665c87ace1022753123fa15bd2d
Oskarmle <121121527+Oskarmle@users.noreply.github.com>
2024-03-03 22:46:36 +0100

    test if working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from "react";
-import Feed from "../components/molecules/Feed";
-import Form from "../components/molecules/Form";
+// import Feed from "../components/molecules/Feed";
+// import Form from "../components/molecules/Form";
+import Form from "@/components/molecules/form";
+import Feed from "@/components/molecules/feed";
 
 export default function index() {
   return (
